479a2de51ecc122c27e4fde4adf3629f18bd739b
Sabari Ganesh <k.sabarii.ganesh@gmail.com>
2021-11-21 15:31:25 +0530

    codingz updated

--- src/components/Codingz.js
+++ src/components/Codingz.js
@@ -9,23 +9,19 @@ import React, { Component } from "react";
 //   Button,
 // } from "reactstrap";
 import demo from "../shared/home/carouselBG.jpg";
-import python from '../shared/codingz/python.png';
-import js from '../shared/codingz/js.png';
-import react from '../shared/codingz/react.png'
-import git from '../shared/codingz/git.png'
-import html from '../shared/codingz/html.png'
-import css from '../shared/codingz/css.png'
+import python from "../shared/codingz/python.png";
+import js from "../shared/codingz/js.png";
+import react from "../shared/codingz/react.png";
+import git from "../shared/codingz/git.png";
+import html from "../shared/codingz/html.png";
+import css from "../shared/codingz/css.png";
 import github from "../shared/codingz/github.png";
 import instagram from "../shared/codingz/instagram.png";
 import hackerrank from "../shared/codingz/hackerrank.png";
-import { Card,Row,Col } from "react-bootstrap";
-
-
-
+import { Card, Row, Col } from "react-bootstrap";
+import Button from "@restart/ui/esm/Button";
 
 function Codingz() {
-
-
   const inCard = "";
   return (
     <div>
@@ -50,82 +46,94 @@ function Codingz() {
           color: "black",
           fontWeight: "bolder",
           fontSize: "2rem",
-          textAlign:'center'
+          textAlign: "center",
         }}
       >
         Here is A GLIMPSE.....
       </div>
-
-<br/> <br/>
-      <div
+      <br /> <br />
+      {/* <div
         style={{
           display: "flex",
           flexDirection: "row",
           textAlign: "center",
           justifyContent: "center",
         }}
+      > */}
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+        }}
       >
-
-       
-        <Card
-          style={{
-            width: "10rem",
-            backgroundColor: "black",
-            borderRadius: "10px",
-            textAlign: "center",
-          }}
-        >
-          <Card.Img
-            variant="top"
-            src={github}
-            style={{ width: "50%"}}
-          />
-          <Card.Body>
-            <Card.Title>OPEN SOURCE</Card.Title>
-          </Card.Body>
-        </Card>
-         
-      
-        <Card
-          style={{
-            width: "10rem",
-            backgroundColor: "darkgreen",
-            borderRadius: "10px",
-            textAlign: "center",
-          }}
-        >
-          <Card.Img
-            variant="top"
-            src={hackerrank}
-            style={{ width: "50%" }}
-          />
-          <Card.Body>
-            <Card.Title>COMPETITIVE CODING</Card.Title>
-          </Card.Body>
-        </Card>
-       
-       
-        <Card
-          style={{
-            width: "10rem",
-            backgroundColor: "#bc2a8d",
-            borderRadius: "10px",
-            textAlign: "center",
-       
-          }}
-        >
-          <Card.Img
-            variant="top"
-            src={instagram}
-            style={{ width: "50%" }}
-          />
-          <Card.Body>
-            <Card.Title>INSTA creations</Card.Title>
-          </Card.Body>
-        </Card>
-      
+        <Row>
+          <Col style={{ padding: "0.5rem" }}>
+            <Card
+              style={{
+                width: "7.5rem",
+                backgroundColor: "black",
+                borderRadius: "8px",
+                textAlign: "center",
+              }}
+            >
+              <Card.Img variant="top" src={github} style={{ width: "50%" }} />
+              <Card.Body>
+                <Card.Title>
+                  OPEN <br /> SOURCE
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ padding: "0.5rem" }}>
+            <Card
+              style={{
+                width: "7.5rem",
+                backgroundColor: "green",
+                borderRadius: "8px",
+                textAlign: "center",
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={hackerrank}
+                style={{ width: "50%" }}
+              />
+              <Card.Body>
+                <Card.Title>COMPETITIVE CODING</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{ padding: "0.5rem" }}>
+            <Card
+              style={{
+                width: "7.5rem",
+                backgroundColor: "#bc2a8d",
+                borderRadius: "8px",
+                textAlign: "center",
+              }}
+            >
+              <Card.Img
+                variant="top"
+                src={instagram}
+                style={{ width: "50%" }}
+              />
+              <Card.Body>
+                <Card.Title>
+                  INSTA <br />
+                  creations
+                </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
       </div>
-          <br/> <br/>
+      <br /> <br />
       <div
         style={{
           fontFamily: "Roboto Slab",
@@ -136,29 +144,96 @@ function Codingz() {
       >
         MY SKILLS INCLUDE
       </div>
-      <br/><br/>
-      <div className = "container" >
-          <Row style = {{textAlign:'left'}}>
-            <Col>
-            <img src = {python} width = "15%" height = "15%" style = {{padding:'20px'}}/>
-            <img src = {js} width = "15%" height = "15%" style = {{padding:'20px'}}/>
-            <img src = {react} width = "15%" height = "15%" style = {{padding:'20px'}}/>
-            </Col>
-          </Row>
-           </div>
-           <br/>
-           <div className = "container" >
-          <Row style = {{textAlign:'right'}}>
-            <Col>
-            <img src = {git} width = "15%" height = "15%" style = {{padding:'20px'}}/>
-            <img src = {html} width = "15%" height = "15%" style = {{padding:'20px'}}/>
-            <img src = {css} width = "15%" height = "15%" style = {{padding:'20px'}}/>
-            </Col>
-          </Row>
-          </div>
-     
-      <div>my works</div>
+      <br />
+      <br />
+      <div className="container">
+        <Row style={{ textAlign: "left" }}>
+          <Col>
+            <img
+              src={python}
+              width="15%"
+              height="15%"
+              style={{ padding: "20px" }}
+            />
+            <img
+              src={js}
+              width="15%"
+              height="15%"
+              style={{ padding: "20px" }}
+            />
+            <img
+              src={react}
+              width="15%"
+              height="15%"
+              style={{ padding: "20px" }}
+            />
+          </Col>
+        </Row>
+      </div>
+      <br />
+      <div className="container">
+        <Row style={{ textAlign: "right" }}>
+          <Col>
+            <img
+              src={git}
+              width="15%"
+              height="15%"
+              style={{ padding: "20px" }}
+            />
+            <img
+              src={html}
+              width="15%"
+              height="15%"
+              style={{ padding: "20px" }}
+            />
+            <img
+              src={css}
+              width="15%"
+              height="15%"
+              style={{ padding: "20px" }}
+            />
+          </Col>
+        </Row>
+      </div>
+      <br />
+      <br />
+      <div
+        className="container"
+        style={{
+          fontFamily: "Roboto Slab",
+          color: "black",
+          fontWeight: "bolder",
+          fontSize: "2rem",
+          textAlign: "center",
+        }}
+      >
+        MY WORKS{" "}
+      </div>
+      <div></div>
       <div>Honours and achievements</div>
+      <div className="container">
+        <Row style={{ textAlign: "center" }}>
+          <Col>
+            <span>PERSONAL WEBSITE</span>
+            <span> &lt; This website/ &gt; </span>
+          </Col>
+          <Col>
+            <span>
+              This is a webite blah blah qhbewuhbidhcbidhchbdihcbdscwncjwdncj
+            </span>
+          </Col>
+          <Col>
+            <span>Uses:- blah, this,that</span>
+          </Col>
+
+          <Col>
+          <Button >VIEW</Button>
+          <Button >GITHUB</Button>
+          </Col>
+        </Row>
+
+        
+      </div>
     </div>
   );
 }
