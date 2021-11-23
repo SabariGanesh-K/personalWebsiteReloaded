a8360aa07a95d3e9deeeec9804030ad71a90ffa4
Sabari Ganesh <k.sabarii.ganesh@gmail.com>
2021-11-23 23:14:06 +0530

    Recent commit about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,53 @@
-
+import vit from "../shared/about/vit.jpg"
+import nsn from "../shared/about/nsn.jpg"
 function About(){
     return (
-        <div>ABOUT</div>
+        <div>
+            <div className = "container">
+                To Brief , 
+                blah blah blah about me
+            </div>
+            <br/>
+            <div >
+                <div style = {{fontFamily:'monospace',fontSize:'2.5rem',textAlign:'center'}}>EDUCATION</div>
+                <br/><br/>
+                <div style = {{background:'rgba(34, 82, 179,0.8)',borderRadius:'4rem',padding:'2rem'}}>
+                <div style = {{fontFamily:'Comfortaa'}}>I am currently pursuing my UG  @ </div>
+                <div style = {{textAlign:'center'}} >
+                        <div>
+                            <br/>
+                        <img src = {vit} width = "70%" />
+                        </div>
+                        <div style = {{fontFamily:'Comfortaa',fontSize:'2rem'}}>
+                       
+                        VIT Chennai <br/>
+                        </div>
+                        <div style = {{fontFamily:'Comfortaa'}}>
+                            (2021-2025)<br/>
+                            Btech in Computer Science and Engineering with specialisation in Cyber Physical Systems
+                        </div>
+                </div>
+                </div>
+                    <br/><br/>
+                <div style = {{background:'rgba(74,30,30,0.8)',borderRadius:'4rem',padding:'2rem'}}>
+                <div style = {{fontFamily:'Comfortaa'}}>I completed my School life  @ </div>
+                <div style = {{textAlign:'center'}} >
+                        <div>
+                            <br/>
+                        <img src = {nsn} width = "70%" />
+                        </div>
+                        <div style = {{fontFamily:'Comfortaa',fontSize:'1.5rem'}}>
+                       
+                        NSN MEMORIAL SENIOR SECONDARY SCHOOL <br/>
+                        </div>
+                        <div style = {{fontFamily:'Comfortaa'}}>
+                            (2006-2021)<br/>
+                            Grade 10 , Grade 12 : C.B.S.E
+                        </div>
+                </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,9 @@ import { Button } from "reactstrap";
 import instagram from "../shared/codingz/instagram.png";
 import dp from "../shared/contact/dp2.png";
 import discord from "../shared/contact/discord.png"
+import medium from "../shared/contact/medium.png"
+import linkedin from "../shared/contact/linkedin.png"
+import twitter from "../shared/contact/twitter.jpg"
 function Contact() {
   return (
     <div>
@@ -9,8 +12,9 @@ function Contact() {
         className="container"
         style={{ fontFamily: "estonia", fontSize: "5rem",display:'flex',justifyContent:'center' }}
       >
-        Lets Catch Up 🎉🎉
+        Lets Catch Up 🎉🎉🎉
       </div>
+      <br/>
       <div className = "container" style = {{display:'flex',justifyContent:'center'}}>
           <img src = {dp} width = "30%" style = {{borderRadius:'15rem'}} />
       </div>
@@ -32,11 +36,35 @@ function Contact() {
       >
         <img src={instagram} width="5%" />
 
-        <Button style={{ borderRadius: "1rem" }}>@beingcodeworm</Button>
+        <Button style={{ borderRadius: "1rem" }}> &nbsp; &nbsp;&nbsp; &nbsp;  @beingcodeworm &nbsp; &nbsp;&nbsp; &nbsp;  </Button>
       </div>
       <div
         className="container"
         style={{ display: "flex", justifyContent: "right", padding: "1rem" }}
+      >
+        <img src={linkedin} width="5%" />
+
+        <Button style={{ borderRadius: "1rem" }}>
+          @sabariganeshk&nbsp; &nbsp; 
+        </Button>
+        
+      </div>
+
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "right", padding: "1rem" }}
+      >
+        <img src={medium} width="5%" />
+
+        <Button style={{ borderRadius: "1rem" }}>
+        @k.sabarii.ganesh
+        </Button>
+        
+      </div>
+
+      <div
+        className="container"
+        style={{ display: "flex", justifyContent: "left", padding: "1rem" }}
       >
         <img src={discord} width="5%" />
 
@@ -48,27 +76,24 @@ function Contact() {
 
       <div
         className="container"
-        style={{ display: "flex", justifyContent: "right", padding: "1rem" }}
+        style={{ display: "flex", justifyContent: "left", padding: "1rem" }}
       >
-        <img src={discord} width="5%" />
+        <img src={twitter} width="5%" />
 
         <Button style={{ borderRadius: "1rem" }}>
-          @SabariGaneshK#1057
+        &nbsp; &nbsp; @SABARISABS1&nbsp; &nbsp; &nbsp; 
         </Button>
         
       </div>
-
+      <br/>
       <div
         className="container"
-        style={{ display: "flex", justifyContent: "right", padding: "1rem" }}
+        style={{ fontFamily: "estonia", fontSize: "5rem",display:'flex',justifyContent:'center' }}
       >
-        <img src={discord} width="5%" />
-
-        <Button style={{ borderRadius: "1rem" }}>
-          @SabariGaneshK#1057
-        </Button>
-        
+        SEE YA ALL ..... :)
       </div>
+      <br/><br/>
+
     </div>
   );
 }
